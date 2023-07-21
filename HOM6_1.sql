f6e22c34ec140d0cc3e7f6402a85d1e88b1416a6
leskovskiy <116192490+leskovskiy@users.noreply.github.com>
2023-07-21 14:38:16 +0700

    Add files via upload

---
+++ HOM6_1.sql
@@ -0,0 +1,29 @@
+DELIMITER $$
+CREATE PROCEDURE times(seconds INT)
+BEGIN
+    DECLARE days INT default 0;
+    DECLARE hours INT default 0;
+    DECLARE minutes INT default 0;
+
+    WHILE seconds >= 84600 DO
+    SET days = seconds / 84600;
+    SET seconds = seconds % 84600;
+    END WHILE;
+
+    WHILE seconds >= 3600 DO
+    SET hours = seconds / 3600;
+    SET seconds = seconds % 3600;
+    END WHILE;
+    
+    WHILE seconds >= 60 DO
+    SET minut = seconds / 60;
+    SET seconds = seconds % 60;
+    END WHILE;
+
+   
+
+SELECT days, hours,minut, seconds;
+END $$
+DELIMITER ;
+
+CALL times(123456);
